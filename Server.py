099e49810497a2cfec96a9109736d497bfac948d
Montreuil <montreuiltommy@gmail.com>
2020-03-20 20:10:39 -0400

    Put the sleep in a thread

--- Server.py
+++ Server.py
@@ -2,24 +2,30 @@ import logging
 import os
 import subprocess
 from datetime import datetime, timedelta
+from threading import Thread
 from time import sleep
 
 
 class Server:
     def __init__(self):
-        self.executable = "nogui.cmd"
+        self._executable = "nogui.cmd"
 
     def command(self, cmd):
         logging.info('Writing server command')
         self.process.stdin.write(str.encode('%s\n' % cmd))
         self.process.stdin.flush()
 
-    def start(self):
+    def initialStart(self):
+        self._start()
+        thread = Thread(target=self._threadedSleep)
+        thread.start()
+
+    def _start(self):
         logging.info('Starting server')
-        self.process = subprocess.Popen(self.executable, stdin=subprocess.PIPE)
+        self.process = subprocess.Popen(self._executable, stdin=subprocess.PIPE)
         logging.info("Server started.")
 
-    def stop(self):
+    def _stop(self):
         logging.info('Stopping server')
         self.command(
             "say Server will close in 1 minute for backup, it will be back online in 1 minute.")
@@ -34,22 +40,25 @@ class Server:
         self.command("stop")
         logging.info('Server stopped')
 
-    def backup(self):
-        server.stop()
+    def _backup(self):
+        server._stop()
         sleep(10)
         os.system("7z a -tzip backup/" +
                   str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S')) + ".zip Chamantopia")
         sleep(5)
-        server.start()
+        server._start()
+
+    def _threadedSleep(self):
+        while True:
+            now = datetime.now()
+            # to = now + timedelta(seconds=1)
+            to = now + timedelta(days=1)
+            to = to.strftime("%Y-%m-%d") + "-7-0-0"
+            to = datetime.strptime(to, '%Y-%m-%d-%H-%M-%S')
+            print("I will now sleep until " + str(to))
+            sleep((to - now).seconds)
+            server._backup()
 
 
 server = Server()
-server.start()
-while True:
-    now = datetime.now()
-    to = now + timedelta(days=1)
-    to = to.strftime("%Y-%m-%d") + "-7-0-0"
-    to = datetime.strptime(to, '%Y-%m-%d-%H-%M-%S')
-    print("I will now sleep until " + str(to))
-    sleep((to - now).seconds)
-    server.backup()
+server.initialStart()
